091e251c91dfe8fedc6c63713af114b0f56242c4
Artyom <cash.kin.art@gmail.com>
2023-06-08 12:04:24 +0300

    tween js => animation

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@
 import { Application, Graphics, Rectangle } from './pixi.mjs';
 import { assetsMap } from './assetsMap.js';
 import { Tank } from './Tank.js';
+import { TweenManager, Tween } from './Tween.js';
 
 // Создаем приложение Pixi.js
 const app = new Application({
@@ -24,6 +25,8 @@ const runGame = () => {
   marker.endFill();
 
   const tank = new Tank();
+  //visible виден элемент или нет
+  tank.view.visible = false;
   app.stage.addChild(tank.view);
   app.stage.addChild(marker);
 
@@ -62,6 +65,45 @@ const runGame = () => {
   app.stage.interactiveChildren = false;
 
   app.stage.hitArea = new Rectangle(-400, -400, 800, 800);
+
+  const rectangle = new Graphics().beginFill(0x000000).drawRect(0, 0, 100, 100).endFill();
+  app.stage.addChild(rectangle);
+
+  /*let value = 0;
+  const stepValue = 0.02;
+  const offset = 200;
+
+
+  app.ticker.add(() => {
+    //lastTime - фиксирует всю продолжительность игры с момента запуска
+    // console.log('app.ticker.lastTime >>', app.ticker.lastTime)
+
+    // console.log('app.ticker.lastTime >>', app.ticker.deltaTime)
+    // console.log('app.ticker.lastTime >>', app.ticker.deltaMS)
+
+    value += stepValue;
+    // прямоуголиник плавно появляется и исчезает - для циклической анимации
+    // rectangle.alpha = Math.cos(value);
+
+    rectangle.position.x = offset * Math.cos(value);
+    // console.log('value >', Math.cos(value));
+
+  })*/
+
+  const tweenManager = new TweenManager(app.ticker);
+
+  window['testTweens'] = {
+    moveTo(duration, position) {
+      tweenManager.createTween(rectangle, duration, position);
+    },
+    rotateTo(duration, rotationData) {
+      tweenManager.createTween(rectangle, duration, rotationData);
+    },
+    moveAndRotate(moveDuration, position, rotationDuration, rotationData) {
+      this.moveTo(moveDuration, position);
+      this.rotateTo(rotationDuration, rotationData);
+    }
+  }
 };
 
 assetsMap.sprites.forEach((value) => app.loader.add(value.name, value.url));
